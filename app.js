94f8625b740366b78d7f505f6ac113d63d96aa4c
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-04-21 18:01:52 +0200

    Feat: Page to calculate numbers

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-import express from 'express';
-//same as const express = require('express');
+import express, { response } from 'express'
+//same as const express = require('express')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 
 app.get('/', (req, res) => {
-    res.send('Welcome to qr-nold. My name is Arnold and I am a QR code generator.');
+    res.send('Welcome to qr-nold. My name is Arnold and I am a QR code generator.')
     }
-);
+)
+
+app.get('/calculate-the-average-of-5-and-10', (request, response) => {
+    const number1 = 5
+    const number2 = 10
+    const average = (number1 + number2) / 2
+    response.send(`The avarage of ${number1} and ${number2} is ${average}`)
+})
+
+app.get('/qr', (req, res) => {
+    res.send('You can now input your vcard')
+}) 
+
+app.post('/qr', (req, res) => {
+    res.send('Thank you for submitting your information, this is your qr-code')
+})
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-    console.log(`http://localhost:${port}`);
-    }
-);
+    console.log(`Server running on port ${port}`)
+    console.log(`http://localhost:${port}`)
+    })
 
