23b0d6ba5b1aa1da0faa0477b552395a05d97a08
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-04-25 15:32:39 +0200

    Feat: Client side js

---
+++ public/js/logger.js
@@ -0,0 +1,10 @@
+const logger = (request, response, next) => {
+  console.log(
+    new Date().toISOString(),
+    'Request from',
+    request.ip,
+    request.method,
+    request.originalUrl
+  )
+  next()
+}
